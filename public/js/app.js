7925664a281d8e470779d6cb9687dc84b6bc0b04
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-12-08 10:09:32 -0700

    api data list format updated

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,9 @@ const locationData = document.getElementById('loc')
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const location = search.value;
+  locationData.innerHTML = ''
+  search.value = ''
   fetch("/weather?address=philadelphia").then(
     (response) => {
       response.json().then((data) => {
@@ -20,21 +23,58 @@ weatherForm.addEventListener("submit", (e) => {
         }
       });
     }
-  );
-  const location = search.value;
-  locationData.innerHTML += location
+    );
+    locationData.innerHTML += location
 });
 
 weatherApiForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const lat = queryLatitude.value;
   const lon = queryLongitude.value;
+  timeData.innerHTML = ''
+  queryLatitude.value = ''
+  queryLongitude.value = ''
   fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=weathercode,sunrise,sunset,windspeed_10m_max&timezone=America%2FSao_Paulo`
   )
     .then((response) => response.json())
-    .then((json) => {
+    .then((json) => {      
       for (obj in json) {
+        timeData.innerHTML += `<li>${obj}:</li>`
+        if(typeof json[obj] !== "object") {
+          timeData.innerHTML += `<ul><li>${json[obj]}</li></ul>`
+        } else {
+          for(item in json[obj]) {
+            timeData.innerHTML += `<ul><li>${item}</li></ul>`
+            if(typeof json[obj][item] !== "object") {
+              timeData.innerHTML += `<ul><ul><li>${json[obj][item]}</li></ul></ul>`
+            }else {
+              for(data in json[obj][item]) {
+                timeData.innerHTML += `<ul><ul><li>${json[obj][item][data]}</li></ul></ul>`                
+                if(data === '4') {
+                  break
+                }
+              }
+            }
+          }
+        }
+      }    
+    });
+});
+
+/* var latitude = "";
+var longitude = "";
+var timeData = document.getElementById("time");
+
+const handleClick = () => {
+    latitude = document.getElementById("input-latitude").value;
+    longitude = document.getElementById("input-longitude").value;
+    fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&daily=weathercode,sunrise,sunset,windspeed_10m_max&timezone=America%2FSao_Paulo`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        for (obj in json) {
         if (typeof json[obj] !== "object") {
           timeData.innerHTML += `<p><strong>${obj}:</strong> ${json[obj]},</p>`;
         } else {
@@ -60,46 +100,5 @@ weatherApiForm.addEventListener("submit", (e) => {
           timeData.innerHTML += `<p>},</p>`;
         }
       }
-    });
-});
-
-/* var latitude = "";
-var longitude = "";
-var timeData = document.getElementById("time");
-
-const handleClick = () => {
-    latitude = document.getElementById("input-latitude").value;
-    longitude = document.getElementById("input-longitude").value;
-    fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&daily=weathercode,sunrise,sunset,windspeed_10m_max&timezone=America%2FSao_Paulo`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        for (obj in json) {
-          if (typeof json[obj] !== "object") {
-            timeData.innerHTML += `<p><strong>${obj}:</strong> ${json[obj]},</p>`;
-          } else {
-            timeData.innerHTML += `<strong><p>${obj}:</strong> <br/> {</p>`;
-            if (typeof json[obj] !== "object") {
-              timeData.innerHTML += `<ul><li>${json[obj]},</li></ul>`;
-            } else {
-              for (item in json[obj]) {
-                if (typeof json[obj][item] !== "object") {
-                  timeData.innerHTML += `<ul><li><strong>${item}:</strong> ${json[obj][item]},</li></ul>`;
-                } else {
-                  timeData.innerHTML += `<ul><li><strong>${item}:</strong><br/> {</li></ul>`;
-                  for (data in json[obj][item]) {
-                    timeData.innerHTML += `<ul><ul><li>${json[obj][item][data]},</li></ul></ul>`;
-                    if (data == 9) {
-                      break;
-                    }
-                  }
-                  timeData.innerHTML += `<ul><li><p>},</p></li></ul>`;
-                }
-              }
-            }
-            timeData.innerHTML += `<p>},</p>`;
-          }
-        }
       });
   }; */
